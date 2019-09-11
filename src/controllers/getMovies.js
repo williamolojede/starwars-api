af1294cdb3736a57d0cba54e9829f5230d756790
William I. Olojede <ilerioluwase97@gmail.com>
2019-09-11 04:13:28 +0100

    move movies sorting into swapi service

--- src/controllers/getMovies.js
+++ src/controllers/getMovies.js
@@ -6,9 +6,6 @@ const { Comment } = models;
 export const getMovies = async () => {
   let movies = await SwapiService.getMovies();
 
-  movies = movies
-    .sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
-
   const episodeIds = movies.map(({ episode_id }) => episode_id);
   
   const commentsCountGroup = await Comment.count({ 
--- src/utils/swapi.js
+++ src/utils/swapi.js
@@ -5,7 +5,8 @@ const SWAPI_FILMS_URL = 'https://swapi.co/api/films';
 export class SwapiService {
   static async getMovies() {
     const { data: { results } } = await axios.get(SWAPI_FILMS_URL);
-    return results;
+    return results
+      .sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
   }
 
   static async getEpisodeCharacters(episodeId) {
