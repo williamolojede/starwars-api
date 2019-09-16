7e4a581cee182ca583495a2db4c69fb77ce5ec11
William I. Olojede <ilerioluwase97@gmail.com>
2019-09-16 10:43:23 +0100

    extract getCharacters method from getEpisodeCharacters

--- src/utils/swapi.js
+++ src/utils/swapi.js
@@ -9,15 +9,17 @@ export class SwapiService {
       .sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
   }
 
-  static async getEpisodeCharacters(episodeId) {
-    let results;
-    const { data: { characters } } = await axios
-      .get(`${SWAPI_FILMS_URL}/${episodeId}`);
-    results = (await Promise.all(
-      characters
-        .map(async (characterUrl) => await axios.get(characterUrl))
-    )).map(({ data }) => data);
+  static async getCharacters(charactersUrls) {
+    const getCharactersRequest = charactersUrls
+      .map(async (characterUrl) => await axios.get(characterUrl));
+    const results = await Promise.all(getCharactersRequest);
+    return results.map(({ data }) => data);
+  }
 
-    return results;
+  static async getEpisodeCharacters(episodeId) {
+    const { data: { characters: charactersUrls } } = await axios
+      .get(`${SWAPI_FILMS_URL}/${episodeId}`);
+    const characters = await SwapiService.getCharacters(charactersUrls);
+    return characters;
   }
 }
